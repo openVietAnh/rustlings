74fcc5dceaa2b602bfe24e2f5a70fbb39c1ef2d1
openVietAnh <vietanhtran.uet@gmail.com>
2025-02-27 11:46:59 +0700

    Solve trais chapter

--- exercises/15_traits/traits2.rs
+++ exercises/15_traits/traits2.rs
@@ -4,6 +4,13 @@ trait AppendBar {
 
 // TODO: Implement the trait `AppendBar` for a vector of strings.
 // `append_bar` should push the string "Bar" into the vector.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Vec<String> {
+        let mut result = self.clone();
+        result.push("Bar".to_string());
+        result
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
